9c2b1273103b724224c004adf2e72c56377cfb9e
yiyulianzhou <449005613@qq.com>
2018-03-30 19:28:10 +0800

    3-30

---
+++ application/controllers/News.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/3/30
+ * Time: 17:59
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+//行情接口
+
+class News extends MY_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        //手动连接数据库
+        $this->load->database();
+    }
+
+    //url http://localhost/api/Quotation/getList?type=1
+    public function getNews()
+    {
+        //type必须传
+        if (empty($_GET['type'])) {
+
+            $this->outData('名称必须',400);
+            exit();
+        }
+
+        //接收参数type
+
+        $type = $this->input->get('type',true);
+
+
+        $this->db->where('type',$type);
+
+        //从quotation表中查出对应的字段
+
+        $data = $this->db->select('exchange_name,last, hight, low, degree, vol,exchange_icon')->get('quotation')->result_array();
+
+        if (empty($data)){
+
+            //获取数据失败
+            $this->outData(0,'获取数据失败','');
+
+        }else{
+
+            //获取数据成功
+            $this->outData(1,'获取数据成功',$data);
+        }
+    }
+
+}
--- application/controllers/Quotation.php
+++ application/controllers/Quotation.php
@@ -11,6 +11,55 @@ class Quotation extends MY_Controller {
 		//手动连接数据库
 		$this->load->database();
 	}
+	//币种接口 url http:10.10.10.109/api/getType
+	public function getType()
+	{
+
+		$data =array(
+			1=>'btc',
+			2=>'eth',
+			3=>'bcd',
+			4=>'elf',
+			5=>'ink',
+			6=>'trx',
+			7=>'aidoc',
+			8=>'pst',
+			9=>'ddd',
+			10=>'eko',
+			11=>'pra',
+			12=>'uc',
+			13=>'aif',
+			14=>'bch',
+			15=>'bcx',
+			16=>'etc',
+			17=>'ltc',
+			18=>'sbtc',
+			19=>'qtum',
+			20=>'neo',
+			21=>'nem',
+			22=>'hsr',
+			23=>'xrp',
+			24=>'dash',
+			25=>'xmr',
+			26=>'eos',
+			27=>'omg',
+			28=>'iota',
+			29=>'zec',
+			30=>'waves',
+			31=>'btg',
+			32=>'bts',
+			33=>'xlm',
+			34=>'lsk'
+		);
+
+		if (empty($data)){
+			//获取数据失败
+			$this->outData(0,'获取数据失败','');
+		}else{
+			//获取数据成功
+			$this->outData(1,'获取数据成功',$data);
+		}
+	}
    //url http://localhost/api/Quotation/getList?type=1
 	public function getList()
 	{
@@ -28,7 +77,8 @@ class Quotation extends MY_Controller {
 
 		$this->db->where('type',$type);
 
-		//从info表中查出对应的字段
+		//从quotation表中查出对应的字段
+
 		$data = $this->db->select('exchange_name,last, hight, low, degree, vol,exchange_icon')->get('quotation')->result_array();
 
 		if (empty($data)){
